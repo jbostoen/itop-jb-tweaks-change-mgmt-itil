d3017fc88434d2d5df67fddb464ecce2fecce2c3
jbostoen <jbostoen@outlook.com>
2020-09-30 15:10:09 +0200

    Version 20200929-1510
    * subrepo

---
+++ en.dict.jb-tweaks-change-mgmt-itil.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2019-2020 Jeffrey Bostoen
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Localized data
+ *
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+
+	'Class:Change/Attribute:reminder' => 'Reminder',
+	
+));
+
+
